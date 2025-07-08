59a283b1a55c9115be2061d6aa628fdf55c11588
Aditya Pratap Singh <adityapratap141003@gmail.com>
2025-07-08 18:37:52 +0530

    index.js file update

--- init/index.js
+++ init/index.js
@@ -1,8 +1,9 @@
+require("dotenv").config({ path: __dirname + "/../.env" });
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const mongo_url = "mongodb://127.0.0.1:27017/wanderlust";
+const dbUrl = process.env.ATLASDB_URL;
 
 main().then(() => {
     console.log("Connected to database");
@@ -11,7 +12,7 @@ main().then(() => {
 });
 
 async function main () {
-    await mongoose.connect(mongo_url);
+    await mongoose.connect(dbUrl);
 };
 
 const initDB = async() => {
